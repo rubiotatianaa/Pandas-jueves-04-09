2fd9e94f8671e65da9d0e878834c667956ef3f32
--global <tatiana789rubio@gmail.com>
2025-09-04 08:44:32 -0500

    Se saca la estadistica de la nueva serie

--- CLASE/Scripts/segundaserie.py
+++ CLASE/Scripts/segundaserie.py
@@ -8,4 +8,10 @@ for i in range(Usuarios):
     dato = int(input(f'Ingrese el dato {i+1}: '))
     SegundaSerie.append(dato)
 
+SegundaSerie = pd.Series(SegundaSerie)
 print(f'Los datos ingresados son: {SegundaSerie}')
+
+print(f'La estadistica de la serie es : {SegundaSerie.describe()}')
+print(f'El orden de la serie es: {SegundaSerie.sort_values()}')
+print(f'El minimo de la serie es: {SegundaSerie.min()}')
+print(f'El maximo de la serie es: {SegundaSerie.max()}')
